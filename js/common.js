da1dac2752ea8f093477d3c5f54539ad8165e7c7
buschidos <sergey.snozyk@gmail.com>
2014-10-15 18:00:08 +0300

    main page

--- js/common.js
+++ js/common.js
@@ -1,8 +1,19 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
+  // $(document).on("click", function(){
+  //  $(".js-popup").hide();
+  // });
 
-	console.log($('body').html());
+  if ( $(".js-slick-slider").length ) {
+    console.log('wow');
+    $(".js-slick-slider").slick({
+      slidesToShow: 4,
+      slidesToScroll: 4,
+      autoplay: true,
+      autoplaySpeed: 5000,
+      slide: '.slider__item',
+      prevArrow: '.slider__prev',
+      nextArrow: '.slider__next',
+    });
+  };
 });
